778224226d7e2ab58b039cf99453439d79a26fde
troy <troymuehlbauer1@gmail.com>
2025-11-22 11:25:26 -0800

    fix merge error

--- frontend/src/pages/ComparisonPage.jsx
+++ frontend/src/pages/ComparisonPage.jsx
@@ -5,8 +5,6 @@ import { appConfig } from "../constants";
 import styles from "../styles/pages/ComparisonPage.module.css";
 import api from "../services/api";
 import LoadingSpinner from "../components/LoadingSpinner";
-import api from "../services/api";
-import LoadingSpinner from "../components/LoadingSpinner";
 
 const ComparisonPage = () => {
   const [selectedTeam, setSelectedTeam] = useState(null); 
@@ -19,60 +17,6 @@ const ComparisonPage = () => {
     // MOCK FUNCTIONALITY - Replace with actual search API call
     console.log("Searching for:", searchTerm);
   };
-
-  const teams = [
-    "None", "Georgia", "Alabama"
-  ]
-
-  useEffect(() => {
-      const fetchTeamData = async () => {
-        setLoading(true);
-        setError(null);
-
-        try {
-          const response = await api.getTeamData("Georgia");
-          if (response.success) {
-            setSelectedTeam(response.data);    
-            console.log(response.data)
-          } else {
-            setError("No team data available.");
-          }
-        } catch (err) {
-          console.error(err);
-          setError("Unable to load team data.");
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchTeamData();
-    }, ["Georgia"]);  
-  
-    if (loading) {
-    return (
-      <div className={styles.comparisonPage}>
-        <Header title={appConfig.name} onSearch={handleSearch} />
-        <main className={styles.comparisonPageMain}>
-          <div className={styles.loadingContainer}>
-            <LoadingSpinner />
-          </div>
-        </main>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className={styles.comparisonPage}>
-        <Header title={appConfig.name} onSearch={handleSearch} />
-        <main className={styles.comparisonPageMain}>
-          <div className={styles.errorContainer}>
-            <p>{error}</p>
-          </div>
-        </main>
-      </div>
-    );
-  }
 
   const teams = [
     "None", "Georgia", "Alabama"
